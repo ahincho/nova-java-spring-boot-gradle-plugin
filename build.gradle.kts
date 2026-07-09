435f7bbd2247a9b70db346a7d49956316c836bfc
ahincho <ahincho@users.noreply.github.com>
2026-07-09 01:14:25 -0500

    feat(gradle): add GPG signing plugin for Maven Central publishing (NOVA-SEMVER-10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     `java-gradle-plugin`
     `maven-publish`
     id("net.nemerosa.versioning") version "4.0.1"
+    id("signing")
 }
 
 versioning {
@@ -50,5 +51,16 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
+    }
+}
+
+signing {
+    val gpgKeyId: String? = System.getenv("GPG_SIGNING_KEY_ID")
+    val gpgKey: String? = System.getenv("GPG_SIGNING_KEY")
+    val gpgPassword: String? = System.getenv("GPG_SIGNING_PASSWORD")
+
+    if (gpgKeyId != null && gpgKey != null) {
+        useInMemoryPgpKeys(gpgKeyId, gpgKey, gpgPassword ?: "")
+        sign(publishing.publications)
     }
 }
